da819098d5a79e4c09dee6ac4feb61bb3cbf78a9
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2025-07-12 22:52:00 +0530

    added the login page

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+import "../styles/form.css";
+import { useNavigate } from "react-router-dom";
+
+const Login = ()=>{
+    const [form, setForm] = useState({username:"", password:""});
+    const [msg, setMsg] = useState("");
+    const navigate = useNavigate();
+
+
+    const handleLogin = async(e)=>{
+        e.preventDefault();
+        try{
+            const res = await axios.post(
+                `${import.meta.env.VITE_API_URL}/user/login`,
+                form
+            );
+
+            localStorage.setItem("token", res.data.token);
+            setMsg("Logged in!");
+            setTimeout(()=> navigate("/board"),1000);
+        }catch(err){
+            setMsg(err.response?.data?.msg || "Login failed");
+        }
+    };
+
+    const handleChange = (e)=>{
+        setForm({ ...form, [e.terget.name]:e.terget.value});
+    };
+
+    return (
+        <div className="form-container">
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                name="username"
+                value={form.username}
+                onChange={handleChange}
+                placeholder="Username"
+                />
+                <input 
+                className="password"
+                type="password" 
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Password"
+                />
+                <button type="submit">Login</button>
+            </form>
+            <p>{msg}</p>
+
+        </div>
+    );
+
+};
+
+export default Login;
+
