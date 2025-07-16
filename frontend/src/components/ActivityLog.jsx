cbd7822f43bb4d1824f1aad7023d39f467d0204c
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2025-07-16 22:35:53 +0530

    fixed bug in Activity log in frontend

--- frontend/src/components/ActivityLog.jsx
+++ frontend/src/components/ActivityLog.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/activityLog.css";
+import "../styles/ActivityLog.css";
 import socket from "../socket";
 
 
